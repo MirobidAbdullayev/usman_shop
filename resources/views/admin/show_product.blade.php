4182da51409856ffd1dce8924c13e420e759b056
Mirobid Abdullayev <80286890+MirobidAbdullayev@users.noreply.github.com>
2022-05-18 15:20:03 +0500

    show product

--- resources/views/admin/show_product.blade.php
+++ resources/views/admin/show_product.blade.php
@@ -3,6 +3,36 @@
   <head>
     <!-- Required meta tags -->
     @include('admin.css')
+
+    <style type="text/css">
+      .center
+      {
+        margin:auto;
+        width: 50%;
+        border: 2px solid white;
+        text-align: center;
+        margin-top: 40px;
+      }
+      .font-size
+      {
+        text-align: center;
+        font-size: 40px;
+        padding-top: 20px;
+      }
+      .img_size
+      {
+        width: 100px;
+        height: 100px;
+      }
+      .th_color
+      {
+        background: skyblue;
+      }
+      .th_deg
+      {
+        padding: 30px;
+      }
+    </style>
   </head>
   <body>
     <div class="container-scroller">
@@ -31,7 +61,33 @@
         <!-- partial -->
         <div class="main-panel">
           <div class="content-wrapper">
-        
+            <h2 class="font-size">Barcha Mahsulotlar</h2>
+            <table class="center">
+              <tr class="th_color">
+                <th class="th_deg">Mahsulot nomi</th>
+                <th class="th_deg">Tasnifi</th>
+                <th class="th_deg">Miqdori</th>
+                <th class="th_deg">Toifasi</th>
+                <th class="th_deg">Narxi</th>
+                <th class="th_deg">Chegirma narxi</th>
+                <th class="th_deg">Mahsulot rasmi</th>
+              </tr>
+              @foreach ($product as $product)
+              <tr>
+                <td>{{$product->title}}</td>
+                <td>{{$product->description}}</td>
+                <td>{{$product->quantity}}</td>
+                <td>{{$product->category}}</td>
+                <td>{{$product->price}}</td>
+                <td>{{$product->discount_price}}</td>
+                <td>
+
+                  <img class="img_size" src="/product/{{$product->image}}">
+
+                </td>
+              </tr>
+              @endforeach
+            </table>
           </div>
         </div>
     <!-- container-scroller -->
